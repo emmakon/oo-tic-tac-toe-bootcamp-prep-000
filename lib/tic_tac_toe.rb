28556a84e5f267896fcc9521f0171908aabb6943
Emma Konrad <emma.konrad92@gmail.com>
2017-10-03 22:51:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ def position_taken?(index)
 end
 
 def valid_move?(index)
-  if index.between?(0,8) && position_taken?(@board, index) == false
+  if index.between?(0,8) && position_taken?(index) == false
   return true
 else
   return false
