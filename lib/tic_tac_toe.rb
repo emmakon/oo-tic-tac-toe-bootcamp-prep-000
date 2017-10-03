016af4466adffe8fa8a54e26c0b800860e9ff24a
Emma Konrad <emma.konrad92@gmail.com>
2017-10-03 22:50:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,5 +42,12 @@ def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
 end
 
+def valid_move?(index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+  return true
+else
+  return false
+end
+end
 
 end
